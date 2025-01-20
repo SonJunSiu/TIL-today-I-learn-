5a4a89e5126f36e4c7405faadbfbae1a0d31f895
SonJunSiu <thswnstn9981@naver.com>
2025-01-20 23:35:04 +0900

    실습

---
+++ Day2-25.01.21/change.py
@@ -0,0 +1,10 @@
+total_books = 10
+
+borrwed_books = 3
+
+left_books = total_books - borrwed_books
+
+print('현재 보유 중인 총 책의 수는 다음과 같습니다')
+print(str(total_books))
+print('그 중, 대여중인 책을 제외한 책의 수는 다음과 같습니다')
+print(str(left_books))
---
+++ Day2-25.01.21/python ex_2_2.py
@@ -0,0 +1,22 @@
+data = {
+    '과목': 'Python',
+    '구분': '실습',
+    '단계': int('2'),
+    '문제번호': int('3251'),
+    '이름': None,
+    '일차': int('22')
+}
+
+print(data)
+
+data['단계'] = str(data['단계']) + '단계'
+
+
+name = '딕셔너리 활용하기'
+data['이름'] = name
+
+
+data['일차'] -= 20
+
+
+print(data)
