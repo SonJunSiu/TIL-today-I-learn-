f4a45b641526b53db84276437623f2c8f08bacfe
SonJunSiu <thswnstn9981@naver.com>
2025-02-10 22:51:53 +0900

    사다리

--- Day11-25.02.10/사다리.py
+++ Day11-25.02.10/사다리.py
@@ -1,4 +1,43 @@
-이동중인 방향 기록
+# 1 은 이동 가능한 길
+# 0 은 이동 불가능한 길
+# 맨 밑바닥 2로 가야한다
+
+# 시작점에서 아래로 진행
+# 그 중 좌, 우 중 갈 수있으면 방향을 바꾸고
+# 다시 아래로 진행
+
+# ex_ 2가 (99, 44) 에 있다면 어디서 시작해야 저기로 가는가
+
+# 1. 일단 사다리에서 2가 있는 위치를 찾느다
+# 2. 2에서 위쪽으로 올라오면서 길을 찾아야한다
+
+def solve (load) :   # 2를 찾는 함수
+    col_num = [] # 2가 들어갈 빈 리스트
+    for i in range(100) : # 행 100회 탐색
+        if load[-1][i] == 2 : # 맨 마지막 행이 2라면 
+            col_num.append(i) # col.num에 추가
+    return col_num
+
+def answer_point(load) : # 2에서 시작해서 첫 지점으로 가는 함수
+    r, c = 99, solve(load)[0] # 100번째(도착점) 에서 시작
+
+    while r > 0 : #행이 1일때 위로 올라감
+        if c > 0 and load[r][c - 1] == 1: # 좌로 이동한다
+            while c > 0 and load[r][c - 1] == 1:
+                c -= 1  # 왼쪽으로 계속 이동
+        elif c < 99 and load[r][c + 1] == 1:
+            while c < 99 and load[r][c + 1] == 1:
+                c += 1  # 오른쪽으로 계속 이동
+
+        r -= 1 # 다시 위로 이동
+
+    return c # 도착 했을때의 열 반환
+
+for _ in range(10):
+    T = int(input())
+    space = [list(map(int,input().split()))for _ in range(100)] # 사다리판 만들기
+
+    real_answer = answer_point(space) # 진짜 정답을 찾는 것
+    print(f'#{T} {real_answer}')
 
 
-한칸 이동 하는 반복문 사용
--- Day11-25.02.10/특별정렬.py
+++ Day11-25.02.10/특별정렬.py
@@ -1,23 +1,30 @@
-def solve (arr, N):
-    for i in range(N-1) : # 제일 작은 값 맨 앞으로 오게 정렬해주기
-        min_idx = i
-        for j in range(i+1, N) : 
-            if arr[min_idx] > arr[j] :
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+def solve(arr, N):
+    for i in range(N - 1):  # 제일 작은 값 맨 앞으로 오게 정렬해주기
+        min_idx = i  # 현재 위치를 최솟값으로
+        for j in range(i + 1, N):  # i 이후 최솟값 비교
+            if arr[min_idx] > arr[j]:  # 지금보다 더 작은 값 찾으면
+                min_idx = j  # 새로 넣어주고
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]  # 스왑하여 최소값 앞으로
 
-        return arr # 정렬된 요소 
-    
-    
+    return arr  # 정렬된 요소 반환
 
-            
+T = int(input())  # 테스트 케이스 개수 입력
+for tc in range(T):
+    N = int(input())  # 배열 크기 입력
+    arr = list(map(int, input().split()))  # N개의 정수 입력받아 리스트로 저장
 
-            
-# 작은거 찾아서 넣고
-# 큰거 찾아서 넣고
-# append 로 붙이기?
-T = int(input())
-N = int(input())
-arr = list(map(int,input().split()))
+    new_arr = solve(arr, N)
 
-print(f'#{T} {result}')
+    arrr = []  # 결과 저장할 리스트
+    first = 0  # 시작 인덱스
+    finish = N - 1  # 마지막 인덱스
+
+    while first <= finish:  # 첫번째랑 마지막이랑 만날 때까지 반복
+        if finish >= first:  # finish가 first보다 크거나 같을 때만 실행
+            arrr.append(arr[finish])  # 큰 값을 먼저 넣어줌
+        if first < finish:  # first와 finish가 같지 않을 때만 실행
+            arrr.append(arr[first])  # 작은 값을 넣어줌
+        first += 1
+        finish -= 1
+
+    print(f"#{tc + 1}", *arrr[:10])  # 최대 10개까지 출력
